848c3f4d07dce33d39b5ad2e5dd7d02b12e448cf
Siddhartha Uppunuti <uppunutisiddhartha@gmail.com>
2026-07-11 01:38:49 +0530

    for demo

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.PROD
     ? "https://clayware-1.onrender.com/api/"
-    : "http://127.0.0.1:8000/api/",
+    : "https://clayware-frontend.vercel.app/",
   headers: {
     "Content-Type": "application/json",
   },
